d5aa909b71c5b454e57680321ad20cb019ba2c28
Olivier D <odonnadei@gmail.com>
2025-03-17 20:31:19 +0000

    Creating workflow WIP

--- AIAgent/agent.py
+++ AIAgent/agent.py
@@ -2,13 +2,18 @@ import os
 import logging
 
 from openai import OpenAI
-from model import WorkoutProgram
-from converter import workout_program_to_json
 
-openapi_key = ''
-with open(os.path.expanduser('~') + '/.openai/api.key') as f:
+from AIAgent.model import AggregatedWorkoutDiet
+from model import WorkoutProgram, Diet, AggregatedWorkoutDiet
+
+openaiapi_key_path = os.path.expanduser('~') + '/.openai/api.key'
+
+with open(openaiapi_key_path) as f:
     openapi_key = f.read().rstrip('\n')
     f.close()
+
+if not openapi_key:
+    raise Exception(f'Could not get OpenAI API key, generate a key and place it in [{openaiapi_key_path}]')
 
 client = OpenAI(api_key=openapi_key)
 
@@ -31,7 +36,26 @@ def get_program(prompt: str):
         ],
         response_format=WorkoutProgram,
     )
-    return workout_program_to_json(completion.choices[0].message.parsed)
+    return completion.choices[0].message.parsed
 
-def extract_program_info(user_input: str) -> WorkoutProgram:
+def validate_program_prompt(user_input: str) -> bool:
+    return False
+
+def generate_diet(program: WorkoutProgram) -> Diet:
     pass
+
+def generate_calendar_invite(program: WorkoutProgram):
+    pass
+
+def generate_program(user_input: str) -> AggregatedWorkoutDiet:
+    # See if program is parsable
+    if validate_program_prompt(user_input):
+        logger.error(f'Gate check failed, could not generate workout program from [{user_input}]')
+        raise Exception('Could not generate program...')
+
+    # Generate program
+    logger.info('Successfully passed gate check, generating workout program !')
+    workout_program = get_program(user_input)
+
+    # Generate Diet and Calendar
+    return AggregatedWorkoutDiet(generate_diet(workout_program), generate_calendar_invite(workout_program))
--- AIAgent/model.py
+++ AIAgent/model.py
@@ -1,19 +1,52 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from converter import workout_program_to_json
 
+# Workout model
 class Weight(BaseModel):
     weight: int
     unit: str
 
 class Exercise(BaseModel):
-    sets: int
-    name: str
-    reps: int
-    weights: Weight
+    sets: int = Field(description='Number of sets to do for the exercise')
+    name: str = Field(description='Name of the exercise')
+    reps: int = Field(description='Number of repetitions to do for the exercise')
+    weights: Weight = Field(description='Weight in kg to carry to perform the exercise')
 
 class Program(BaseModel):
-    day: str
-    exercises: list[Exercise]
+    day: str = Field(description='The day the exercise list will be executed')
+    exercises: list[Exercise] = Field(description='The list of workout exercise to be performed on the day of the workout')
 
 class WorkoutProgram(BaseModel):
-    participants: list[str]
-    program: list[Program]
+    participants: list[str] = Field(description='The workout participants')
+    program: list[Program] = Field(description='The program to be executed by the participants')
+    objective: str = Field(description='The objective of the participants')
+    def __str__(self):
+        return workout_program_to_json(self)
+
+# Diet model
+class Recipe(BaseModel):
+    ingredients: list[str] = Field(description='Ingredients of the recipe')
+    steps: list[str] = Field(description='Steps to cook the recipe')
+
+class DailyDiet(BaseModel):
+    breakfast: Recipe = Field(description='Recipe to eat at breakfast')
+    lunch: Recipe = Field(description='Recipe to eat at lunch time')
+    dinner: Recipe = Field(description='Recipe to eat for dinner')
+
+class Diet(BaseModel):
+    week: list[DailyDiet] = Field(description='List of daily meals to prepare to optimise the workout effects')
+
+# Aggregated model
+class AggregatedWorkoutDiet:
+    workout: WorkoutProgram
+    diet: Diet
+
+    def __init__(self, workout: WorkoutProgram, diet: Diet):
+        self.workout = workout
+        self.diet = diet
+
+# Gate model
+class WorkoutProgramExtraction(BaseModel):
+    description: str = Field(description='Raw description of the workout program with the participants objective')
+    is_workout_program: bool = Field(description='Whether description describes a workout program with an objective')
+    confidence_score: float = Field(description='Confidence score between 1 and 0')
